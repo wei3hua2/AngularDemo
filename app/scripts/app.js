03bb59d2cbb7d3c15f3d126486e5df32cdec5b0a
James Chong <james.chong@troytech.net>
2014-04-04 09:00:54 +0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularDemoApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
